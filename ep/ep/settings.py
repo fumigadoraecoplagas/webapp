6e74ffd3c839ba2c7c4eabf638eb4d37139c3c10
ppaniaguas <pablo.paniagua@ecoplagascr.com>
2024-04-13 12:57:11 -0600

    credenciales

--- ep/ep/settings.py
+++ ep/ep/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import firebase_admin
+from firebase_admin import credentials
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+cred = credentials.Certificate('credentials.json')
+firebase_admin.initialize_app(cred)
