92e80e2cbab30b2c81277d65965efa86b80577dc
ppaniaguas <pablo.paniagua@ecoplagascr.com>
2024-05-31 21:44:53 -0600

    Fix static tag and update templates

--- ecobot/urls.py
+++ ecobot/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('ventas/', include('ventas.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='ventas/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
-    path('', auth_views.LoginView.as_view(template_name='ventas/login.html')),  # Redirigir al login
+    path('', views.index, name='index'),
 ]
