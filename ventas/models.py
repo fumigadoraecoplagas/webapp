08e7b4ddd081a6fc8780a2c44c430aa1c8609c21
ppaniaguas <pablo.paniagua@ecoplagascr.com>
2024-05-31 21:24:46 -0600

    Fix import issues and define Venta model

--- ventas/models.py
+++ ventas/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class Venta(models.Model):
+    canton = models.CharField(max_length=100)
+    provincia = models.CharField(max_length=100)
+    nombre = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=20)
+    precio_colones = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha_inicio = models.DateTimeField()
+    fecha_fin = models.DateTimeField()
+    color_ruta = models.CharField(max_length=20, choices=[
+        ('Verde', 'Verde'),
+        ('Morada', 'Morada'),
+        ('Amarilla', 'Amarilla'),
+        ('Naranja', 'Naranja'),
+        ('Celeste', 'Celeste'),
+        ('Rosada', 'Rosada'),
+        ('Café', 'Café')
+    ])
+    servicios = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.nombre
